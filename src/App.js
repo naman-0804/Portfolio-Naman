61117cff82ed8a42a9abcd43015b8da25d03bae4
Naman Srivastava <namansrivastava1608@gmail.com>
2024-06-22 14:40:15 +0530

    Added button style

--- src/App.js
+++ src/App.js
@@ -37,10 +37,10 @@ function App() {
       <div className="navbar">
         <div className="section">
           <h2>Welcome to my Portfolio</h2>
-          <button id="p" onClick={() => scrollToSection('project-section')}>Project</button>
-          <button id="s" onClick={() => scrollToSection('skills-section')}>Skills</button>
-          <button id="e" onClick={() => scrollToSection('experience-section')}>Experience</button>
-          <button id="c" onClick={() => scrollToSection('contact-section')}>Contact</button>
+          <button id="p" className='button-86' onClick={() => scrollToSection('project-section')}>Project</button>
+          <button id="s" className='button-86' onClick={() => scrollToSection('skills-section')}>Skills</button>
+          <button id="e" className='button-86' onClick={() => scrollToSection('experience-section')}>Experience</button>
+          <button id="c" className='button-86' onClick={() => scrollToSection('contact-section')}>Contact</button>
         </div>
       </div>
       <div className="container" style={{ backgroundColor: "lime" }}>
@@ -60,24 +60,24 @@ function App() {
           <div className="vitalized">
             <h1>Vitalized</h1>
             <img decoding="async" src="https://fileinfo.com/img/ss/xl/jpg_44-2.jpg" width="600px" height="250px" alt="Vitalized" />
-            <button id="vitalized-button" onClick={() => redirectToProject('vitalized-button')}>View project</button>
+            <button id="vitalized-button" className='button-81' onClick={() => redirectToProject('vitalized-button')}>View project</button>
           </div>
           <div className="asl">
             <h1>ASL/ISL Translator</h1>
             <img decoding="async" src="https://fileinfo.com/img/ss/xl/jpg_44-2.jpg" width="600px" height="250px" alt="ASL/ISL Translator" />
-            <button id="asl-button" onClick={() => redirectToProject('asl-button')}>View project</button>
+            <button id="asl-button" className='button-81' onClick={() => redirectToProject('asl-button')}>View project</button>
           </div>
         </div>
         <div>
           <div className="medhub">
             <h1>Medhub360</h1>
             <img decoding="async" src="https://fileinfo.com/img/ss/xl/jpg_44-2.jpg" width="600px" height="250px" alt="Medhub360" />
-            <button id="medhub-button" onClick={() => redirectToProject('medhub-button')}>View project</button>
+            <button id="medhub-button" className='button-81' onClick={() => redirectToProject('medhub-button')}>View project</button>
           </div>
           <div className="okra">
             <h1>Okra</h1>
             <img decoding="async" src="https://fileinfo.com/img/ss/xl/jpg_44-2.jpg" width="600px" height="250px" alt="Okra" />
-            <button id="okra-button" onClick={() => redirectToProject('okra-button')}>View project</button>
+            <button id="okra-button" className='button-81' onClick={() => redirectToProject('okra-button')}>View project</button>
           </div>
         </div>
       </div>
