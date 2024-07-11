1ca7fac73ccf2c532edd00e8d4056c0c2177da1c
un-identified <namansrivastava1608@gmail.com>
2024-07-11 18:42:05 +0530

    fixed experience tab

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,23 +2,33 @@ import React from 'react';
 
 function Experience() {
   return (
-    <div id="experience-section" className="exed" style={{ backgroundColor: "pink" }}>
+    <div id="experience-section" className="exed">
       <center><h1>Experience</h1></center>
-      <div className="exp">
-        <h3>Software Developer</h3>
-        <h3>Software Developer</h3>
-        <h3>Software Developer</h3>
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
+      <ul className="exp">
+        <li>
+          <h3>Software Development Intern at TEN</h3>
+          <p>Worked as a software development intern at The Entrepreneurship Network (TEN), contributing to various projects and gaining valuable industry experience.</p>
+          <b><p><strong>Technologies:</strong> React, Flask, MongoDb</p></b>
+        </li>
+        <li>
+          <h3>Web Developer at Biosphere Club</h3>
+          <p>Active member of the Biosphere Club, responsible for developing and maintaining the club's website, ensuring it is up-to-date and user-friendly.</p>
+          <b><p><strong>Technologies:</strong> HTML, CSS, JavaScript, Python</p></b>
+        </li>
+      </ul>
       <br />
       <h1>Education</h1>
 
       <div className="edu">
-        <h3>College VIT</h3>
-        <h3>High School</h3>
+        <h3>College - Vellore Institute of Technology (2022-2026)</h3>
+        <ul>
+          <li>Pursuing B.Tech in Computer Science and Engineering</li>
+        </ul>
+        <h3>School - Peace Public School (2020-2022)</h3>
+        <ul>
+          <li>Completed 10th in 2020 with 90%</li>
+          <li>Completed 12th in 2022 with 90.4%</li>
+        </ul>
       </div>
     </div>
   );
