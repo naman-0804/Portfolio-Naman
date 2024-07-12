707fe8de09eaa7e3a884551cdc91a5580beb7110
un-identified <namansrivastava1608@gmail.com>
2024-07-12 07:16:41 +0530

    Changing look of home

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,10 +15,11 @@ function Home() {
         <a href="https://github.com/un-identified" target="_blank" rel="noopener noreferrer">
           <img align="center" src="https://skillicons.dev/icons?i=github" alt="Naman" height="50" width="50" style={{ margin: '10px' }} />
         </a>
-
+        <br></br><center>
         <a href={resume} download="Naman_Srivastava_Resume.pdf">
           <button style={{ margin: '10px', padding: '10px 20px', fontSize: '16px' }}>Download Resume</button>
         </a>
+        </center>
       </div> 
       <div className="dp">
         <img src={photo} alt="Profile" />
