ac4dad4a431e17970643d8fd24a85ca04b58a348
un-identified <namansrivastava1608@gmail.com>
2024-07-12 07:05:12 +0530

    added resume and profile links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import photo from '../profile-pic.jpg';
+import resume from '../Naman_srivastava.pdf'
 function Home() {
   return (
     <div className="profile-container" style={{ }}>
@@ -7,11 +8,23 @@ function Home() {
         <h1>I am Naman</h1>
         <h1>I am a Software Developer</h1>
         <h3>I am more passionate about developing new websites and applications</h3>
+        <a href="https://linkedin.com/in/naman1608" target="_blank" rel="noopener noreferrer">
+          <img align="center" src="https://skillicons.dev/icons?i=linkedin" alt="Naman" height="50" width="50" />
+        </a> 
+        
+        <a href="https://github.com/un-identified" target="_blank" rel="noopener noreferrer">
+          <img align="center" src="https://skillicons.dev/icons?i=github" alt="Naman" height="50" width="50" style={{ margin: '10px' }} />
+        </a>
+
+        <a href={resume} download="Naman_Srivastava_Resume.pdf">
+          <button style={{ margin: '10px', padding: '10px 20px', fontSize: '16px' }}>Download Resume</button>
+        </a>
       </div> 
-      <div className='dp'>
-      <img decoding="async"  src="https://fileinfo.com/img/ss/xl/jpg_44-2.jpg" alt="Profile"  />
+      <div className="dp">
+        <img src={photo} alt="Profile" />
       </div>
     </div>
   );
 }
+
 export default Home;
