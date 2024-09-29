e610276afb6bc454d1d6426d4023bd43000cccbd
un-identified <namansrivastava1608@gmail.com>
2024-09-29 09:46:57 +0530

    updated the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,25 +3,31 @@ import okrapic from '../Images/okra-website.png';
 import vitalized from '../Images/vitalized-website.png';
 import medhub from '../Images/medhub360-website.png';
 import sl from '../Images/SL-website.png';
-
+import sahyogi from '../Images/sahyogi.png';
+import aws from '../Images/AWS.png';
 function Projects() {
   function redirectToProject(id) {
     let url = '';
     switch (id) {
       case 'vitalized-button':
-        url = 'https://github.com/un-identified/vitalized-vit';
+        url = 'https://github.com/naman-0804/vitalized-vit';
         break;
       case 'asl-button':
-        url = 'https://github.com/un-identified/Sign-Language-Translator';
+        url = 'https://github.com/naman-0804/Sign-Language-Translator';
         break;
       case 'medhub-button':
-        url = 'https://github.com/un-identified/Healthcare';
+        url = 'https://github.com/naman-0804/Healthcare';
         break;
       case 'okra-button':
-        url = 'https://github.com/un-identified/Okra-Maturity-Analysis';
+        url = 'https://github.com/naman-0804/Okra-Maturity-Analysis';
+        break;
+      case 'sahyogi-button':
+        url = 'https://github.com/naman-0804/SIH';
+        break;
+      case 'AWS-button':
+        url = 'https://github.com/naman-0804/Cloud-AWS';
         break;
       default:
-        break;
     }
     if (url) {
       window.open(url, '_blank');
@@ -38,9 +44,14 @@ function Projects() {
           <button id="vitalized-button" className='button' onClick={() => redirectToProject('vitalized-button')}>View project</button>
         </div>
         <div className="project-item">
-          <h1>ASL/ISL Translator</h1>
-          <img src={sl} alt="ASL/ISL Translator" />
-          <button id="asl-button" className='button' onClick={() => redirectToProject('asl-button')}>View project</button>
+          <h1>Sahyogi</h1>
+          <img src={sahyogi} alt="Sahyogi" />
+          <button id="sahyogi-button" className='button' onClick={() => redirectToProject('sahyogi-button')}>View project</button>
+        </div>
+        <div className="project-item">
+          <h1>Serverless Web App on AWS</h1>
+          <img src={aws} alt="AWS" />
+          <button id="AWS-button" className='button' onClick={() => redirectToProject('AWS-button')}>View project</button>
         </div>
         <div className="project-item">
           <h1>Medhub360</h1>
@@ -48,10 +59,16 @@ function Projects() {
           <button id="medhub-button" className='button' onClick={() => redirectToProject('medhub-button')}>View project</button>
         </div>
         <div className="project-item">
+          <h1>ASL/ISL Translator</h1>
+          <img src={sl} alt="ASL/ISL Translator" />
+          <button id="asl-button" className='button' onClick={() => redirectToProject('asl-button')}>View project</button>
+        </div>
+        <div className="project-item">
           <h1>Okra Maturity Analysis</h1>
           <img src={okrapic} alt="Okra" />
           <button id="okra-button" className='button' onClick={() => redirectToProject('okra-button')}>View project</button>
         </div>
+        
       </div>
     </div>
   );
