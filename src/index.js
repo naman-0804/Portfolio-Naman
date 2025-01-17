c905c5aba6b3903566ed042e0ed6a4f3bc1499d6
un-identified <namansrivastava1608@gmail.com>
2025-01-17 23:29:17 +0530

    analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.title = 'Naman';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <Analytics />
     <App />
   </React.StrictMode>
 );
