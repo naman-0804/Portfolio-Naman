1e802e0861f5d49284daa92b28301705d0558e78
un-identified <namansrivastava1608@gmail.com>
2025-01-17 23:25:20 +0530

    added tracking

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <Router>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Analytics } from "@vercel/analytics/react"
 document.title = 'Naman';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
