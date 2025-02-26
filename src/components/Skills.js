7b3583352be2b89179599927ec2f27d7d1dd0bf6
un-identified <namansrivastava1608@gmail.com>
2025-02-26 09:46:36 +0530

    more changes skill css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -27,44 +27,36 @@ function Skills() {
       <center>
         <h1 className="section-title">Achievements</h1>
       </center>
-      <table className="achievements-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {[
-            {
-              title: "Software Developer Intern",
-              description:
-                "Secured a summer internship with The Entrepreneurship Network and learned a lot of new skills.",
-            },
-            {
-              title: "Devshouse 4th Place",
-              description:
-                "Participated in the Devshouse competition and secured the 4th place among 600+ teams.",
-            },
-            {
-              title: "Solveathon 6th Place",
-              description:
-                "Ranked 6th in the Solveathon and received special appreciation for our idea.",
-            },
-            {
-              title:
-                "VITISH (SIH Internal Hackathon) Nominated for Official SIH24",
-              description:
-                "Qualified two elimination rounds and got nominated for official SIH24 from VIT Chennai out of 600 teams.",
-            },
-          ].map((achievement, index) => (
-            <tr key={index}>
-              <td>{achievement.title}</td>
-              <td>{achievement.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="Achievements">
+        {[
+          {
+            title: "Software Developer Intern",
+            description:
+              "Secured a summer internship with The Entrepreneurship Network and learned a lot of new skills.",
+          },
+          {
+            title: "Devshouse 4th Place",
+            description:
+              "Participated in the Devshouse competition and secured the 4th place among 600+ teams.",
+          },
+          {
+            title: "Solveathon 6th Place",
+            description:
+              "Ranked 6th in the Solveathon and received special appreciation for our idea.",
+          },
+          {
+            title:
+              "VITISH (SIH Internal Hackathon) Nominated for Official SIH24",
+            description:
+              "Qualified two elimination rounds and got nominated for official SIH24 from VIT Chennai out of 600 teams.",
+          },
+        ].map((achievement, index) => (
+          <div key={index} className="achievement-card">
+            <h3>{achievement.title}</h3>
+            <p>{achievement.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
