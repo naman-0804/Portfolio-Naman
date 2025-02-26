3667640b420e60073c4b22129d3ae0c294f1b1ee
un-identified <namansrivastava1608@gmail.com>
2025-02-26 10:35:37 +0530

    fixed expandedu

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,35 +2,33 @@ import React from 'react';
 
 function Experience() {
   return (
-    
-    <div id="experience-section" className="exed" style={{ backgroundColor: '#fff7e6' }}>
-      <center><h1>Experience</h1></center>
+    <div id="experience-section" className="exed">
+      <h1>Experience</h1>
       <ul className="exp">
         <li>
           <h3>Software Development Intern at TEN</h3>
           <p>Worked as a software development intern at The Entrepreneurship Network (TEN), contributing to various projects and gaining valuable industry experience.</p>
-          <b><p><strong>Technologies:</strong> React, Flask, MongoDb</p></b>
+          <p><strong>Technologies:</strong> React, Flask, MongoDB</p>
         </li>
         <li>
           <h3>Web Developer at Biosphere Club</h3>
           <p>Active member of the Biosphere Club, responsible for developing and maintaining the club's website, ensuring it is up-to-date and user-friendly.</p>
-          <b><p><strong>Technologies:</strong> HTML, CSS, JavaScript, Python</p></b>
+          <p><strong>Technologies:</strong> HTML, CSS, JavaScript, Python</p>
         </li>
       </ul>
-      <br />
-      <h1>Education</h1>
 
-      <div className="edu">
-        <h3>College - Vellore Institute of Technology (2022-2026)</h3>
-        <ul>
-          <li>Pursuing B.Tech in Computer Science and Engineering</li>
-        </ul>
-        <h3>School - Peace Public School (2020-2022)</h3>
-        <ul>
-          <li>Completed 10th in 2020 with 90%</li>
-          <li>Completed 12th in 2022 with 90.4%</li>
-        </ul>
-      </div>
+      <h1>Education</h1>
+      <ul className="edu">
+        <li>
+          <h3>College - Vellore Institute of Technology (2022-2026)</h3>
+          <p>Pursuing B.Tech in Computer Science and Engineering</p>
+        </li>
+        <li>
+          <h3>School - Peace Public School (2020-2022)</h3>
+          <p>Completed Class X in 2020 with 90%</p>
+          <p>Completed Class XII in 2022 with 90.4%</p>
+        </li>
+      </ul>
     </div>
   );
 }
